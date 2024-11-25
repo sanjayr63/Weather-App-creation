2c7de826574c460c8d11d7552c0f449468759f6f
sanjayr63 <sanjayngmc12@gmail.com>
2024-11-25 11:13:01 +0530

    Weather app creation

---
+++ Weather_App/weatherproject/weatherapp/form.py
@@ -0,0 +1,12 @@
+# forms.py
+from django import forms
+from django.forms import ModelForm, TextInput
+from .models import City
+
+class CityForm(ModelForm):
+    class Meta: 
+        model = City 
+        fields = ['name']  
+        widgets = {
+            'name': TextInput(attrs={'class': 'form-control', 'placeholder': 'City Name'}) 
+        }
